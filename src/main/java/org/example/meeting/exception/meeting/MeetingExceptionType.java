b4805bc11b2deb3b32d2b29e0f6b23dc43fd7ab6
201912025 <michi2012@naver.com>
2025-01-03 14:04:39 +0900

    feat: 회의예외타입 추가

--- src/main/java/org/example/meeting/exception/meeting/MeetingExceptionType.java
+++ src/main/java/org/example/meeting/exception/meeting/MeetingExceptionType.java
@@ -8,7 +8,8 @@ import org.example.global.exception.ExceptionType;
 @AllArgsConstructor
 public enum MeetingExceptionType implements ExceptionType {
 
-    NOT_FOUND_MEETING(9000, "화상통화를 찾을 수 없습니다.");
+    NOT_FOUND_MEETING(8000, "화상통화를 찾을 수 없습니다."),
+    NOT_CREATE_MEETING(8010, "화상통화를 생성할 수 없습니다.");
 
     private final int statusCode;
     private final String message;
