e4bcbba3edc38aff517353e93f1b7aefa786e7af
201912025 <michi2012@naver.com>
2025-02-15 13:03:50 +0900

    feat: healthcheck ì¶가

--- src/main/java/org/example/global/common/HealthCheckController.java
+++ src/main/java/org/example/global/common/HealthCheckController.java
@@ -1,5 +1,6 @@
 package org.example.global.common;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class HealthCheckController {
     @GetMapping("/")
     public String healthCheck() {
         return "the service is up and running ...";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Service is healthy");
     }
 }
