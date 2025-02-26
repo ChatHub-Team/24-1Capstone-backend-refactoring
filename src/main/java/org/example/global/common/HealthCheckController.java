a96b435163f8843ae1ffb8d4a40689444b4badf6
201912025 <michi2012@naver.com>
2025-02-26 20:18:52 +0900

    test: cicd test

--- src/main/java/org/example/global/common/HealthCheckController.java
+++ src/main/java/org/example/global/common/HealthCheckController.java
@@ -18,6 +18,6 @@ public class HealthCheckController {
 
     @GetMapping("/api/health")
     public ResponseEntity<String> checkApiHealth() {
-        return ResponseEntity.ok("server is running-ci-test");
+        return ResponseEntity.ok("server is running-cicd-test");
     }
 }
